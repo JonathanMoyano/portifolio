cba170afd1924d78445ada826043dc81d572f8dd
JonathanPolezel <jsouza1993@gmail.com>
2024-11-16 01:01:07 -0300

    atualizações

---
+++ next-seo.config.js
@@ -0,0 +1,10 @@
+export default {
+  title: 'Jonathan Souza Moyano',
+  description: 'Portfólio profissional de Jonathan Souza Moyano',
+  openGraph: {
+    title: 'Jonathan Souza Moyano',
+    description: 'Portfólio profissional de Jonathan Souza Moyano',
+    type: 'website',
+    locale: 'pt_BR',
+  },
+};
--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,13 +1,12 @@
-// src/pages/_document.jsx
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="pt-BR">
       <Head>
         <meta charSet="utf-8" />
-        <link rel="icon" href="/favicon.ico" />
-        {/* Você pode adicionar mais meta tags, links de fontes, etc. aqui */}
+        <link rel="icon" href="/favicon.jpeg" />
+        {/* Outras meta tags e links */}
       </Head>
       <body>
         <Main />
